731e9bc1d9e6d3628d913084543b1bdde9512145
EricSimWP <110527948+EricSimWP@users.noreply.github.com>
2023-08-01 10:19:03 +0800

    Added search input with auto complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,15 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import usePlacesAutocomplete, {getGeocode, getLatLng} from "use-places-autocomplete";
+import {
+	Combobox,
+	ComboboxInput,
+	ComboboxPopover,
+	ComboboxList,
+	ComboboxOption,
+	ComboboxOptionText,
+} from "@reach/combobox"; // to display results and user input 
+import "@reach/combobox/styles.css";
 
 function Home() {
     const { isLoaded } = useLoadScript({
@@ -11,20 +21,69 @@ function Home() {
     return <Map />
 }
 
-export default Home;
-
 function Map() {
     const center = useMemo (() => ({lat:1.418000, lng: 103.828650}), []);
+    const [selected, setSelected] = useState([]);
+    console.log(selected);
     return (
         <>
+            <div className="places-container">
+                <PlacesAutoComplete setSelected={setSelected} />
+            </div>
+
             <GoogleMap 
                 zoom={10} 
                 center={center} 
                 mapContainerClassName="map-container"
             >
-                <Marker position={center} />
+                {selected.map((x) => <Marker position={x} />)} 
             </GoogleMap>
         </>
     )
 }
 
+function PlacesAutoComplete( {setSelected } ) {
+    const {
+        ready,
+        value,
+        setValue,
+        suggestions: {status, data},
+        clearSuggestions
+    } = usePlacesAutocomplete();
+    
+    const handleSelect = async (address) => {
+        setValue(address, false);
+        clearSuggestions(); 
+
+        // address -> geocode -> lat lng 
+        
+        const results = await getGeocode( {address} );
+        console.log(`results: ${results}`)
+        const {lat, lng} = await getLatLng(results[0]);
+        setSelected((list) => [...list, {lat, lng}])
+    }
+
+    return (
+        <Combobox onSelect={handleSelect}>
+            <ComboboxInput 
+                value={value} 
+                onChange={e => setValue(e.target.value)} 
+                disabled={!ready}
+                className="combobox-input"
+                placeholder="Search an address"
+            />
+            <ComboboxPopover>
+                <ComboboxList>
+                    {status === "OK" && data.map(({place_id, description}) => 
+                        <ComboboxOption 
+                            key={place_id}
+                            value={description}
+                        />
+                    )}
+                </ComboboxList>
+            </ComboboxPopover>
+        </Combobox>
+    )
+}
+
+export default Home;
